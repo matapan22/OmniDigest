1b890ce930266ef94ffc65d51fdcaee7144fe329
matapan3070 <matapan1998@gmail.com>
2025-11-26 11:06:10 +0100

    switch to work in progress
    switch to work in progress

--- Frondend/src/components/Header/menuData.tsx
+++ Frondend/src/components/Header/menuData.tsx
@@ -33,7 +33,7 @@ const menuData: Menu[] = [
       {
         id: 42,
         title: "Youtube Video Summary",
-        path: "/error",
+        path: "/workinprogress",
         newTab: false,
       },
     ],
